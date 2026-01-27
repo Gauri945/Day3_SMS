2c793860199e4bedac9df42559baf6627358d430
Gauri <cgauri915@gmail.com>
2026-01-27 15:25:12 +0530

    CRUD operations update

--- src/main/java/com/example/day3_sms/controller/StudentController.java
+++ src/main/java/com/example/day3_sms/controller/StudentController.java
@@ -2,18 +2,20 @@ package com.example.day3_sms.controller;
 
 
 import com.example.day3_sms.model.StudentModel;
+import com.example.day3_sms.repository.StudentRepository;
 import com.example.day3_sms.service.StudentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
    private final StudentService service;
+    private final StudentRepository studentRepository;
 
-    public StudentController(StudentService service) {
+    public StudentController(StudentService service, StudentRepository studentRepository) {
         this.service = service;
+        this.studentRepository = studentRepository;
     }
 
     //create function API
@@ -21,4 +23,23 @@ public class StudentController {
     public StudentModel addStudent(@RequestBody StudentModel student){
         return service.addStudent(student);
     }
+
+    //Display or read
+    @GetMapping("/students")
+    public List<StudentModel> getStudents(){
+        return service.getStudents();
+    }
+
+    //update (PUT)
+    @PutMapping("/update/{id}")
+    public StudentModel updateStudent(@PathVariable String id , @RequestBody StudentModel student){
+        return service.updateStudent(id,student);
+    }
+
+    //Delete
+    @DeleteMapping("/deleteId/{id}")
+    public StudentModel deleteById(@PathVariable String id){
+        return service.deleteById(id);
+    }
+
 }
--- src/main/java/com/example/day3_sms/service/StudentService.java
+++ src/main/java/com/example/day3_sms/service/StudentService.java
@@ -3,6 +3,10 @@ package com.example.day3_sms.service;
 import com.example.day3_sms.model.StudentModel;
 import com.example.day3_sms.repository.StudentRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -15,5 +19,30 @@ public class StudentService {
     //Create
     public StudentModel addStudent(StudentModel student){
         return repository.save(student);
+    }
+
+    //Display Student
+
+    public List<StudentModel> getStudents(){
+        return repository.findAll();
+    }
+
+    //Update
+    public StudentModel updateStudent(String id , StudentModel student){
+        StudentModel existingStudent = repository.findById(id)
+                .orElseThrow(()-> new RuntimeException("No Student Found"));
+        existingStudent.setName(student.getName());
+        existingStudent.setAge(student.getAge());
+        existingStudent.setEmail(student.getEmail());
+
+        return repository.save(existingStudent);
+
+    }
+
+    public StudentModel deleteById(String id){
+        StudentModel existingid = repository.findById(id)
+                .orElseThrow(()-> new RuntimeException("No ID Found"));
+        repository.deleteById(id);
+        return existingid;
     }
 }
